d89db4648ef6cd66a299c0542ad7dae1bf793dfa
KeyLo99 <keylo99official@gmail.com>
2018-06-25 17:42:34 +0200

    decodeApk method created using apktool.jar

--- src/binder/Binder.java
+++ src/binder/Binder.java
@@ -1,5 +1,46 @@
 package binder;
 
+import brut.apktool.Main;
+import brut.common.BrutException;
+
+import java.io.*;
+
 public class Binder {
+
+    public String decodeApk(File apkFile){
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            PrintStream printStream = System.out;
+            System.setOut(new PrintStream(byteArrayOutputStream));
+            if(apkFile.exists()) {
+                Main.main(new String[]{"d", "-f", apkFile.getPath().trim()});
+            }else {
+                throw new Exception("No readable apk file found.");
+            }
+            System.out.flush();
+            System.setOut(printStream);
+            return byteArrayOutputStream.toString();
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return ex.getMessage();
+        }
+    }
+    public String readFile(String filePath){
+        StringBuilder content = new StringBuilder();
+        String line;
+        File file = new File(filePath);
+        if(file.exists()){
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(file.getPath()));
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                }
+                in.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return content.toString();
+    }
 
 }
--- src/main/ApkBinder.java
+++ src/main/ApkBinder.java
@@ -2,8 +2,17 @@ package main;
 
 import binder.Binder;
 
-public class ApkBinder {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class ApkBinder {
+    private static String workingDir = System.getProperty("user.dir");
+    private static Binder binder;
+    public static void main(String[] args) {
+        binder = new Binder();
+        String logs = binder.decodeApk(new File(workingDir+"\\testfile.apk"));
+        System.out.println(logs);
     }
 }
